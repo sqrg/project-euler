ef256c0f53b672e12be44c211596d4682bf33c80
Santiago Quiroga <santiago.j.quiroga@gmail.com>
2015-03-24 17:42:22 -0300

    Problema 1

---
+++ problema_1.py
@@ -0,0 +1,12 @@
+def es_multiplo_de(n, m):
+    """
+    Devuelve True si n es multiplo de m
+    """
+    return n%m==0
+
+lista = []
+for n in range(1000):
+    if es_multiplo_de(n,3) or es_multiplo_de(n, 5):
+        lista.append(n)
+
+print sum(lista)
